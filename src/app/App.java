df6411fdcb4c80b44be9600a03e098d2a22bef45
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2021-10-19 14:24:41 -0400

    Fix

--- src/app/App.java
+++ src/app/App.java
@@ -7,11 +7,7 @@ package app;
 public class App {
 
     public static void main(String... args) {
-        Player character = new Player();
-        character.getInventory().addCoins(Coin.GOLD,243);
-        System.out.println(character.getInventory().getMoneyWeight());
-        character.getInventory().balanceMoney();
-        System.out.println(character.getInventory().getMoneyWeight());
+
     }
 
 }
