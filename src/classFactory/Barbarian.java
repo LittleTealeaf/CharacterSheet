07871f10b3621d3d1ddcaf10acdceb4aa59ee900
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2021-10-22 11:30:41 -0400

    Changes to Barbarian Class

--- src/classFactory/Barbarian.java
+++ src/classFactory/Barbarian.java
@@ -30,8 +30,17 @@ public class Barbarian extends PlayerClass {
                                 new Bonus(BarbarianAttributes.RAGE_MAX,2),
                                 new Bonus(BarbarianAttributes.RAGE_DAMAGE,2)
                         };
-                        description =
-                                "In battle, you fight with primal ferocity. On your turn, you can enter a rage as a bonus action.\n" + "\n" + "While raging, you gain the following benefits if you aren't wearing heavy armor:\n" + "\n" + "You have advantage on Strength checks and Strength saving throws.\n" + "When you make a melee weapon attack using Strength, you gain a +2 bonus to the damage roll. This bonus increases as you level.\n" + "You have resistance to bludgeoning, piercing, and slashing damage.\n" + "If you are able to cast spells, you can't cast them or concentrate on them while raging.\n" + "\n" + "Your rage lasts for 1 minute. It ends early if you are knocked unconscious or if your turn ends and you haven't attacked a hostile creature since your last turn or taken damage since then. You can also end your rage on your turn as a bonus action.";
+                        description = """
+                                      In battle, you fight with primal ferocity. On your turn, you can enter a rage as a bonus action.
+
+                                      While raging, you gain the following benefits if you aren't wearing heavy armor:
+
+                                      You have advantage on Strength checks and Strength saving throws.
+                                      When you make a melee weapon attack using Strength, you gain a +2 bonus to the damage roll. This bonus increases as you level.
+                                      You have resistance to bludgeoning, piercing, and slashing damage.
+                                      If you are able to cast spells, you can't cast them or concentrate on them while raging.
+
+                                      Your rage lasts for 1 minute. It ends early if you are knocked unconscious or if your turn ends and you haven't attacked a hostile creature since your last turn or taken damage since then. You can also end your rage on your turn as a bonus action.""";
                         name = "Rage";
                         classLevel = 1;
                     }
@@ -181,8 +190,10 @@ public class Barbarian extends PlayerClass {
             @Override
             void initialize() {
                 name = "Relentless Rage";
-                description = "Your rage can keep you fighting despite grievous wounds. If you drop to 0 hit points while you're raging and don't " +
-                        "die outright, you can make a DC 10 Constitution saving throw. If you succeed, you drop to 1 hit point instead.\n" + "\n" + "Each time you use this feature after the first, the DC increases by 5. When you finish a short or long rest, the DC resets to 10.";
+                description = """
+                              Your rage can keep you fighting despite grievous wounds. If you drop to 0 hit points while you're raging and don't die outright, you can make a DC 10 Constitution saving throw. If you succeed, you drop to 1 hit point instead.
+
+                              Each time you use this feature after the first, the DC increases by 5. When you finish a short or long rest, the DC resets to 10.""";
                 classLevel = 11;
             }
         }, new ClassFeature() {
